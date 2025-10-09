f8e85895c4df4bf8a1511b6d8ac2ff090b0593f6
tecnomus <tecnomus2@gmail.com>
2025-10-08 23:28:18 -0700

    Update Blink_182_without_delay.ino
    flex comment

--- Blink_182_without_delay.ino
+++ Blink_182_without_delay.ino
@@ -2,14 +2,14 @@
 
 
 void setup() {
- 
+  // Set digital pins 13, 12, 11, and 10 as outputs.
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
 }
 
-
+//In this part we have a loop 
 void loop() {
   
   digitalWrite(13, HIGH);  //Allow the power for the pin function to on the led
@@ -18,9 +18,9 @@ void loop() {
 
        
 
-    digitalWrite(12, HIGH);  
+    digitalWrite(12, HIGH);  //Allow the power for the pin function to on the led
                       
-  digitalWrite(12, LOW);  
+  digitalWrite(12, LOW);  //Disactivate the power for the pin function to off the led
            
 
     digitalWrite(11, HIGH);  
@@ -34,4 +34,5 @@ void loop() {
  
 
 }
+
 
