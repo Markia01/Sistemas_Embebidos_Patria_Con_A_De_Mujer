c1471c916822cc46833255c0d78eb366aa1a470a
Kaleb <hekamartinezhe@gmail.com>
2025-10-08 23:09:40 -0700

    add comments on blink

--- Blink-182.ino
+++ Blink-182.ino
@@ -1,35 +1,35 @@
+// This function runs once when the Arduino board is reset or powered on.
+void setup()
+{
 
-
-
-void setup() {
- 
+  // Set digital pins 13, 12, 11, and 10 as outputs.
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
 }
 
+// This function runs over and over again in a loop, forever.
+void loop()
+{
 
-void loop() {
-  
-  digitalWrite(13, HIGH);  
-                  
-  digitalWrite(13, LOW);  
-       
+  // Turn pin 13 ON.
+  digitalWrite(13, HIGH);
+  // Immediately turn pin 13 OFF.
+  digitalWrite(13, LOW);
 
-    digitalWrite(12, HIGH);  
-                      
-  digitalWrite(12, LOW);  
-           
+  // Turn pin 12 ON.
+  digitalWrite(12, HIGH);
+  // Immediately turn pin 12 OFF.
+  digitalWrite(12, LOW);
 
-    digitalWrite(11, HIGH);  
-                    
-  digitalWrite(11, LOW);  
-   
+  // Turn pin 11 ON.
+  digitalWrite(11, HIGH);
+  // Immediately turn pin 11 OFF.
+  digitalWrite(11, LOW);
 
-    digitalWrite(10, HIGH);  
-                     
-  digitalWrite(10, LOW);  
- 
-
+  // Turn pin 10 ON.
+  digitalWrite(10, HIGH);
+  // Immediately turn pin 10 OFF.
+  digitalWrite(10, LOW);
 }
