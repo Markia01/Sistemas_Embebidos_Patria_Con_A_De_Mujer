0c428e2566203ac996ae2be0f466d20f072841fb
Markia01 <tic-320124@utnay.edu.mx>
2025-10-08 23:16:48 -0700

    Add files via upload

---
+++ blink_182_with_delay.ino
@@ -0,0 +1,34 @@
+void setup()
+{
+
+  // Set digital pins 13, 12, 11, and 10 as outputs.
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+// This function runs over and over again in a loop, forever.
+void loop()
+{
+
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+
+  digitalWrite(12, HIGH);
+  delay(1000);
+  digitalWrite(12, LOW);
+  delay(1000);
+
+  digitalWrite(11, HIGH);
+  delay(1000);
+  digitalWrite(11, LOW);
+  delay(1000);
+
+  digitalWrite(10, HIGH);
+  delay(1000);
+  digitalWrite(10, LOW);
+  delay(1000);  
+}
