269400c40912834827c7644a679da609212b8161
Kaleb <hekamartinezhe@gmail.com>
2025-11-30 19:50:46 -0700

    feat(dht): eliminar Lab-4.ino y agregar Lab-5.ino - integración LCD y DHT11

---
+++ Lab-5.ino
@@ -0,0 +1,12 @@
+#include <LiquidCrystal.h>
+#include <DHT.h>
+
+// --- CONFIGURACIÓN DEL LCD ---
+// Pines: RS(12), E(11), D4(5), D5(4), D6(3), D7(2)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// --- CONFIGURACIÓN DEL SENSOR DHT ---
+#define DHTPIN 7      // Pin digital donde conectaste el cable de DATOS del sensor
+#define DHTTYPE DHT11 // Si tu sensor es BLANCO, cambia DHT11 por DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
